bc69f5af6e6c05d9836a816c3f67886d0934e52b
Diego <diegomoretto@uol.com.br>
2017-01-25 11:21:57 -0200

    Changing the get declarations of the sitemapmanagerconfiguration class

--- Sitemap.XML/Models/SitemapManagerConfiguration.cs
+++ Sitemap.XML/Models/SitemapManagerConfiguration.cs
@@ -19,21 +19,17 @@
  *                                                                         *
  * *********************************************************************** */
 
-using System.Runtime.InteropServices;
 using System.Web;
 using System.Xml;
 using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Xml;
-using System.Collections.Specialized;
 using Sitecore.Diagnostics;
 using System.Linq;
 using System.Collections.Generic;
 using Sitecore;
-using Sitecore.Sites;
 using Sitecore.Data.Fields;
-using Sitecore.Links;
 
 namespace Sitemap.XML.Models
 {
@@ -43,7 +39,7 @@ namespace Sitemap.XML.Models
 
         #endregion
 
-        #region Constructor 
+        #region Constructor
 
         public SitemapManagerConfiguration(string siteName)
         {
@@ -88,9 +84,21 @@ namespace Sitemap.XML.Models
             }
         }
 
-        public bool HasWildcardItems => GetValueByName("hasWildcardItems") == "true";
+        public bool HasWildcardItems
+        {
+            get
+            {
+                return GetValueByName("hasWildcardItems") == "true";
+            }
+        }
 
-        public string WildcardRoutesPath => GetValueByName("wildcardRoutesPath");
+        public string WildcardRoutesPath
+        {
+            get
+            {
+                return GetValueByName("wildcardRoutesPath");
+            }
+        }
 
         public bool GenerateRobotsFile
         {
@@ -101,17 +109,29 @@ namespace Sitemap.XML.Models
             }
         }
 
-        public string EnabledTemplates => GetValueByNameFromDatabase(Constants.WebsiteDefinition.EnabledTemplatesFieldName);
+        public string EnabledTemplates
+        {
+            get
+            {
+                return GetValueByNameFromDatabase(Constants.WebsiteDefinition.EnabledTemplatesFieldName);
+            }
+        }
 
-        public bool CleanupBucketPath => GetValueByNameFromDatabase(Constants.WebsiteDefinition.CleanupBucketPath) == "1";
+        public bool CleanupBucketPath
+        {
+            get
+            {
+                return GetValueByNameFromDatabase(Constants.WebsiteDefinition.CleanupBucketPath) == "1";
+            }
+        }
 
         public string ServerUrl
         {
             get
             {
-                var url =  GetValueByNameFromDatabase(Constants.WebsiteDefinition.ServerUrlFieldName);
-                return string.IsNullOrWhiteSpace(url) ? HttpContext.Current.Request.Url.Scheme+"://" 
-                    +Context.Site.Properties["hostname"] : url.Trim('/');
+                var url = GetValueByNameFromDatabase(Constants.WebsiteDefinition.ServerUrlFieldName);
+                return string.IsNullOrWhiteSpace(url) ? HttpContext.Current.Request.Url.Scheme + "://"
+                    + Context.Site.Properties["hostname"] : url.Trim('/');
             }
         }
 
@@ -128,14 +148,20 @@ namespace Sitemap.XML.Models
         {
             get
             {
-                return GetCustomPathItem(Constants.WebsiteDefinition.CustomStartPath);               
-               
+                return GetCustomPathItem(Constants.WebsiteDefinition.CustomStartPath);
+
             }
         }
 
-        public string SiteName { get; } = string.Empty;
+        public string SiteName { get; set; }
 
-        public string FileName => GetValueByNameFromDatabase(Constants.WebsiteDefinition.FileNameFieldName);
+        public string FileName
+        {
+            get
+            {
+                return GetValueByNameFromDatabase(Constants.WebsiteDefinition.FileNameFieldName);
+            }
+        }
 
         #endregion properties
 
@@ -177,19 +203,19 @@ namespace Sitemap.XML.Models
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        private  string GetCustomPathItem(string item)
+        private string GetCustomPathItem(string item)
         {
             string result = string.Empty;
 
             Database db = Factory.GetDatabase(WorkingDatabase);
             if (db != null)
             {
-                
+
                 var configItem = db.Items[SitemapConfigurationItemPath];
                 if (configItem != null)
                 {
                     var reference = (ReferenceField)configItem.Fields[Constants.WebsiteDefinition.CustomStartPath];
-                    if(reference != null && reference.TargetItem != null)
+                    if (reference != null && reference.TargetItem != null)
                     {
                         //return LinkManager.GetItemUrl(reference.TargetItem, new UrlOptions { Site = SiteContext.Current, SiteResolving = false, LanguageEmbedding = LanguageEmbedding.Never });
                         return reference.TargetItem.Paths.FullPath;
@@ -210,7 +236,7 @@ namespace Sitemap.XML.Models
             var configRoot = Factory.GetDatabase(WorkingDatabase).GetItem(sitemapXmlSystemRootId);
             if (configRoot == null) return null;
 
-            var configs = configRoot.Children.Where(i=>i.TemplateName!="Folder");
+            var configs = configRoot.Children.Where(i => i.TemplateName != "Folder");
             if (!configs.Any()) return null;
 
             var siteNames = configs.Select(c => c.Name);
